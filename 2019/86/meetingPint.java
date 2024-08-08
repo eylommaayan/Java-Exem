54d245c16943d35eafd205237516aac3640c0acf
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-08-08 15:09:31 +0300

    הצחלה שאלה

---
+++ 2019/86/meetingPint.java
@@ -0,0 +1,20 @@
+public class meetingPint {
+
+    public static void main(String[] args) {
+        
+        
+    }
+
+    public static int meetingPint(int[] a, int[] b) {
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = Math.min(a.length, b.length)-1;
+   
+        while (start <= end) {
+            int mid = (start+end)/2;
+            if (a[mid] == b[mid])
+
+        }
+        
+    }
+}
