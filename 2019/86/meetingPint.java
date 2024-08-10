099dba7aeea97af19aad3d4b11891ede8cc34d73
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-08-10 22:00:16 +0300

    עדכון

--- 2019/86/meetingPint.java
+++ 2019/86/meetingPint.java
@@ -1,20 +1,25 @@
 public class meetingPint {
 
-    public static void main(String[] args) {
-        
-        
-    }
 
-    public static int meetingPint(int[] a, int[] b) {
-        int min = Integer.MAX_VALUE;
-        int start = 0;
-        int end = Math.min(a.length, b.length)-1;
-   
-        while (start <= end) {
-            int mid = (start+end)/2;
-            if (a[mid] == b[mid])
-
+    public static int meetingPoint(int[] a, int[] b) {
+        int low = 0, 
+        mid = 0, 
+        minIndex = -1;
+        int high = Math.min(a.length - 1, b.length - 1);
+        
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (a[mid] == b[mid]) {
+                minIndex = mid;
+                high = mid - 1;
+            } else if (a[mid] > b[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
         
+        return minIndex;
     }
+    
 }
